6ccdd7a2d90b0239bb4b5456fbb60536990d0952
figoxu <xujh945@qq.com>
2018-09-09 14:47:39 +0800

    调整java的GRPC编译命令

--- pb/release_proto.sh
+++ pb/release_proto.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 protoc -I . --go_out=plugins=grpc:./sso ./api.proto
 protoc -I . --java_out=./java ./api.proto
+protoc --plugin=protoc-gen-grpc-java=/Users/xujianhui/develop/golang/gopath/src/github.com/grpc/grpc-java/compiler/build/exe/java_plugin/protoc-gen-grpc-java \
+  --grpc-java_out=lite:"./java" --proto_path="$DIR_OF_PROTO_FILE" "./api.proto"
 protoc --doc_out=./doc --doc_opt=html,index.html ./api.proto
 protoc --doc_out=./doc --doc_opt=markdown,README.md ./api.proto
