cceb997ca7ceed0fece5507d283ee8960678a191
figoxu <xujh945@qq.com>
2018-09-09 11:31:49 +0800

    初始化SSO FILTER

--- client/java.localhost/src/main/java/me/figoxu/sso/SampleApplication.java
+++ client/java.localhost/src/main/java/me/figoxu/sso/SampleApplication.java
@@ -1,10 +1,13 @@
 package me.figoxu.sso;
 
+import me.figoxu.sso.filter.SsoFilter;
 import me.figoxu.sso.util.SpringContextUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @ServletComponentScan
@@ -14,4 +17,15 @@ public class SampleApplication {
         SpringContextUtil.setApplicationContext(context);
     }
 
+
+    @Bean
+    public FilterRegistrationBean ssoFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(new SsoFilter());
+        registration.addUrlPatterns("/*");
+        registration.addInitParameter("paramName", "paramValue");
+        registration.setName("ssoFilter");
+        registration.setOrder(1);
+        return registration;
+    }
 }
---
+++ client/java.localhost/src/main/java/me/figoxu/sso/filter/SsoFilter.java
@@ -0,0 +1,102 @@
+package me.figoxu.sso.filter;
+
+import me.figoxu.sso.web.MainController;
+import org.apache.log4j.Logger;
+
+import javax.servlet.*;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLEncoder;
+
+/**
+ * Created by xujianhui on 2018/9/9.
+ */
+public class SsoFilter implements Filter {
+    Logger logger = Logger.getLogger(SsoFilter.class);
+
+    public static final String redirect_url = "http://sso.localhost/sso/login/redirect";
+    public static final String SSO_BASIC_PURE_TOKEN = "basic_raw_token";
+    public static final String SSO_TOKEN_COOKIE = "sso";
+    public static final String SSO_TOKEN_PARAM = "basic_pure_token";
+    public static final String SSO_FROM_PARAM = "from";
+    public static final String MINE_DOMAIN = "java.localhost";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    private void resolveRedirectUrl(HttpServletRequest req, HttpServletResponse rsp) throws IOException {
+        String currentURL = req.getRequestURL().toString();
+        String queryString = req.getQueryString();
+        if (queryString != null && queryString.length() > 0) {
+            currentURL = currentURL + "?" + queryString;
+        }
+        logger.info(currentURL);
+        logger.info(URLEncoder.encode(currentURL, "UTF-8"));
+        String redirectURL = redirect_url + "?from=" + URLEncoder.encode(currentURL, "UTF-8");
+        rsp.sendRedirect(redirectURL);
+    }
+
+    private void autoSaveToken(HttpServletRequest req, HttpServletResponse rsp) throws IOException {
+        String token = req.getParameter(SSO_TOKEN_PARAM);
+        if (token == null || token.length() <= 0) {
+            return;
+        }
+        Cookie cookie = new Cookie(SSO_TOKEN_COOKIE, token);
+        cookie.setPath("/");
+        cookie.setMaxAge(60 * 60 * 24);
+        cookie.setDomain(MINE_DOMAIN);
+        rsp.addCookie(cookie);
+        req.getSession().setAttribute(SSO_BASIC_PURE_TOKEN, token);
+    }
+
+    private String getBasicPureToken(HttpServletRequest req, HttpServletResponse rsp) throws IOException {
+        String token = req.getParameter(SSO_TOKEN_PARAM);
+        if (token != null && token.length() > 0) {
+            System.out.println("GET TOKEN FROM REQUEST");
+            return token;
+        }
+        for (Cookie cookie : req.getCookies()) {
+            if (cookie.getName().equals(SSO_TOKEN_COOKIE)) {
+                token = cookie.getValue();
+                if (token != null && token.length() > 0) {
+                    System.out.println("GET TOKEN FROM COOKIE");
+                    return token;
+                }
+            }
+        }
+        token = (String) req.getSession().getAttribute(SSO_BASIC_PURE_TOKEN);
+        return token;
+    }
+
+    private Boolean isLogin(HttpServletRequest req, HttpServletResponse rsp) throws IOException {
+        String token = getBasicPureToken(req, rsp);
+        if (token == null || token.length() <= 0) {
+            return false;
+        }
+        //todo check with grcp
+        return true;
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse rsp = (HttpServletResponse) servletResponse;
+        autoSaveToken(req, rsp);
+        if (!isLogin(req, rsp)) {
+            resolveRedirectUrl(req, rsp);
+        }else{
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+
+}
