0758aca07820e2f6c4779b99a90fa73141dc6afc
figoxu <xujh945@qq.com>
2018-09-10 18:19:40 +0800

    修复由于middleware执行顺序导致session报错问题

--- client/go.localhost/web.go
+++ client/go.localhost/web.go
@@ -20,11 +20,11 @@ func initWeb(port string) {
 
 func mount() *gin.Engine {
 	r := gin.Default()
+	store := cookie.NewStore([]byte("xujh945@qq.com"))
+	r.Use(sessions.Sessions("figoxu", store))
 	ssoMid := common.BuildMid(sysEnv.domain, sysEnv.sso_redirect_url, sysEnv.grpc_client, sysEnv.token_cache)
 	r.Use(ssoMid, static.Serve("/dist", static.LocalFile("./dist", true)))
 	r.HTMLRender = pongo2gin.Default()
-	store := cookie.NewStore([]byte("xujh945@qq.com"))
-	r.Use(sessions.Sessions("figoxu", store))
 	r.Use(ssoMid)
 	r.GET("/main", h_main)
 	return r
